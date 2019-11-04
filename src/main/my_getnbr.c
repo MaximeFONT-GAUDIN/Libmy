22b10b7f7cde7035951808626d4ec7167791f1b4
MaximeFONT-GAUDIN <maxime.font-gaudin@epitech.eu>
2019-11-04 16:18:17 +0100

    issues #3 and #4 solved

--- src/main/my_getnbr.c
+++ src/main/my_getnbr.c
@@ -10,18 +10,18 @@
 int my_getnbr(char *str)
 {
     int nb = 0;
-    int n = 0;
+    long int n = 0;
 
     for (int i = 0; str[i] != 0; ++i) {
         if (str[i] == '-')
             nb++;
-        else if (str[i] == '+')
-            ++i;
         if (str[i] >= '0' && str[i] <= '9')
             n = n * 10 + (str[i] - '0');
         else if (str[i] >= 'a' && str[i] <= 'z')
             break;
     }
+    if (n > 2147483648)
+        return (0);
     if (nb % 2 == 1)
         return (n * (-1));
     return (n);
---
+++ suppr.c
@@ -0,0 +1,8 @@
+#include <limits.h>
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d\n", INT_MIN);
+	return (0);
+}
