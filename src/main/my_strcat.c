26f0a1f12ee6020f648301478484e7b19217b6ec
MaximeFONT-GAUDIN <maxime.font-gaudin@epitech.eu>
2019-11-05 12:03:56 +0100

    isses #11 and #12 solved

--- src/main/my_strcat.c
+++ src/main/my_strcat.c
@@ -9,20 +9,12 @@
 
 char *my_strcat(char *str1, char *str2)
 {
-    char *ret;
-    int size_ret;
-    int size_str1 = 0;
-    int size_str2 = 0;
-    int save = 0;
+    int size_str1 = my_strlen(str1);
 
-    for (; str1[size_str1] != 0; size_str1++);
-    for (; str2[size_str2] != 0; size_str2++);
-    size_ret = size_str1 + size_str2 + 1;
-    ret = malloc(sizeof(char) * size_ret);
-    for (; str1[save] != 0; save++)
-        ret[save] = str1[save];
-    for (size_str2 = 0; str2[size_str2] != 0; size_str2++, save++)
-        ret[save] = str2[size_str2];
-    ret[save] = 0;
-    return (ret);
+    for (int i = 0; str2[i] != 0; i++) {
+        str1[size_str1] = str2[i];
+        size_str1++;
+    }
+    str1[size_str1] = 0;
+    return (str1);
 }
--- src/test/my_strcat_test.c
+++ src/test/my_strcat_test.c
@@ -7,19 +7,48 @@
 
 #include "my_test.h"
 #include <criterion/criterion.h>
+#include <criterion/redirect.h>
 
 Test(my_strcat, concat_correctly)
 {
-    char *str1 = "Hello";
-    char *str2 = "World";
-    
-    cr_assert_str_eq(my_strcat(str1, str2), "HelloWorld");
+    char str1[11] = "Hello\0";
+    char *str2 = "World\0";
+
+    my_strcat(str1, str2);
+    cr_assert_str_eq(str1, "HelloWorld");
 }
 
 Test(my_strcat, concat_different_size)
 {
-    char *str1 = "Little";
+    char str1[10] = "Little";
     char *str2 = "Big";
 
     cr_assert_str_eq(my_strcat(str1, str2), "LittleBig");
+}
+
+Test(my_strcat, empty_str1)
+{
+    char str1[11] = "";
+    char *str2 = "HelloWorld";
+
+    my_strcat(str1, str2);
+    cr_assert_str_eq(str1, "HelloWorld");
+}
+
+Test(my_strcat, empty_str2)
+{
+    char str1[11] = "HelloWorld";
+    char *str2 = "";
+
+    my_strcat(str1, str2);
+    cr_assert_str_eq(str1, "HelloWorld");
+}
+
+Test(my_strcat, same_pointer)
+{
+    char str1[11] = "Hello";
+    char *str2 = "World";
+    char *r = my_strcat(str1, str2);
+
+    cr_assert_eq(str1, r);
 }
