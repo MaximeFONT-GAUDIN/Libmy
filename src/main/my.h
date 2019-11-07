606507137b6face7cc3939f81d43e64a1974c695
MaximeFONT-GAUDIN <maxime.font-gaudin@epitech.eu>
2019-11-07 22:33:46 +0100

    my_compute_square_root.c and test

--- src/main/my.h
+++ src/main/my.h
@@ -51,5 +51,6 @@ int my_isneg(int n);
 int my_is_prime(int nb);
 int my_find_prime_sup(int nb);
 int my_compute_power_rec(int nb, int p);
+int my_compute_square_root(int nb);
 
 #endif
---
+++ src/main/my_compute_square_root.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2019
+** my_compute_square_root.c
+** File description:
+** my_compute_square_root.c
+*/
+
+#include "my.h"
+
+int my_compute_square_root(int nb)
+{
+    for (int i = 0; i <= nb / 2 + 1; i++) {
+        if (i * i == nb)
+            return (i);
+    }
+    return (0);
+}
