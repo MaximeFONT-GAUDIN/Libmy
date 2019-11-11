6e7a082c42bc093b5e72ef55f42f8c686292abf2
MaximeFONT-GAUDIN <maxime.font-gaudin@epitech.eu>
2019-11-11 01:59:01 +0100

    add test NULL string my_strstr.c

--- src/test/my_strstr_test.c
+++ src/test/my_strstr_test.c
@@ -59,6 +59,14 @@ Test(my_strstr, occurence_is_the_last)
 Test(my_strstr, find_empty_string)
 {
     char *str = "on va charcher une empty string ici";
+    char *r = my_strstr(str, "");
+
+    cr_assert_eq(r, NULL);
+}
+
+Test(my_strstr, find_NULL_string)
+{
+    char *str = "on va charcher une empty string ici";
     char *r = my_strstr(str, NULL);
 
     cr_assert_eq(r, NULL);
