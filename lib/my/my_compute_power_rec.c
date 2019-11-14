02e56958baabe3c96f6a08bddefbfe2a76250a7a
MaximeFONT-GAUDIN <maxime.font-gaudin@epitech.eu>
2019-11-14 15:02:56 +0100

    update power_rec

--- lib/my/my_compute_power_rec.c
+++ lib/my/my_compute_power_rec.c
@@ -11,11 +11,11 @@ int my_compute_power_rec(int nb, int p)
 {
     int nb1, nb2;
 
-    if (nb == 0 || p < 0)
-        return (0);
-
     if (p == 0)
         return (1);
+
+    if (nb == 0 || p < 0)
+        return (0);
 
     nb1 = my_compute_power_rec(nb, p - 1);
     nb2 = nb * nb1;
